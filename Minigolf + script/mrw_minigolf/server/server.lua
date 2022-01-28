9aa48ace036f4e71e6d2e1c5083740f185de493d
Viloll <92787590+Viloll22@users.noreply.github.com>
2022-01-28 10:54:48 +0100

    Add files via upload

---
+++ Minigolf + script/mrw_minigolf/server/server.lua
@@ -0,0 +1,27 @@
+-- Author : Morow
+-- Github : https://github.com/Morow73
+
+if Config.USE_ESX then
+    ESX = nil
+    TriggerEvent('esx:getSharedObject', function( obj ) ESX = obj end)
+end
+
+RegisterNetEvent("mrw_minigolf:locateClub")
+AddEventHandler("mrw_minigolf:locateClub", function()
+    local x_source = source
+
+    if Config.USE_ESX then
+        local xPlayer = ESX.GetPlayerFromId(x_source)
+        local getPlayerMoney = xPlayer.getMoney()
+
+        if getPlayerMoney >= Config.club_price then
+            TriggerClientEvent("mrw_minigolf:st_game", x_source, 1)
+        else
+            TriggerClientEvent("mrw_golf:Notification", x_source, translation["no_money"])
+        end
+    else
+        -- IF USE YOUR CUSTOM FRAMEWORK !!
+        -- ADD YOUR LINE FOR COMPARE PLAYER MONEY
+        TriggerClientEvent("mrw_minigolf:st_game", x_source, 1)
+    end
+end)
---
+++ Minigolf + script/mrw_minigolf/shared/translation/en.lua
@@ -0,0 +1,17 @@
+if language == "en" then
+    translation = {
+        ["congrats"] = "Felicidades",
+        ["round_win"] = "Terminaste este hoyo con ",
+        ["finish_game"] = "Se acabo el juego, hiciste un total de : ",
+        ["stroke"] = "Tiro",
+        ["locate_club"] = "~INPUT_CONTEXT~ para rentar palos por $200 !",
+        ["other_params"] = "~INPUT_ATTACK~ para cargar el golpe !\n ~INPUT_VEH_FLY_ATTACK_CAMERA~ mostrar los puntos ! \n",
+        ["rotate_params"] = "~INPUT_CELLPHONE_LEFT~ ~INPUT_CELLPHONE_RIGHT~ rotar alrededor de la bola !\n",
+        ["games_params"] = "~INPUT_VEH_PREV_RADIO~ para reiniciar el nivel !\n ~INPUT_CELLPHONE_OPTION~ quitar el juego !",
+        ["no_money"] = "No tienes fondos !",
+        ["max_stroke"] = "Superaste el maximo de golpes en este round",
+        ["quit"] = "Saliste del juego",
+        ["off_side"] = "La bola salio del campo",
+        ["play_your"] = "Total de Tiros: "
+    }
+end
